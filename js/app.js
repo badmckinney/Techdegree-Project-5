3e3be4c0b6d756074e087455e5d8be97f5be3bdb
Brad McKinney <brad.mckin@gmail.com>
2018-07-18 19:19:39 -1000

    Added a fetch function and check status function

---
+++ js/app.js
@@ -0,0 +1,34 @@
+const employees = document.getElementById('employees');
+
+const fetchData = (url) => {
+  return fetch(url)
+    .then(checkStatus)
+    .then(res => res.json())
+    .catch(error => console.log('Looks like there was a problem', error))
+}
+
+const checkStatus = (response) => {
+  if (response.ok) {
+    return Promise.resolve(response);
+  } else {
+    return Promise.reject(new Error(response.statusText));
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fetchData('https://randomuser.me/api/')
+  .then(res => console.log(res))
