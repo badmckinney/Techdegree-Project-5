8a767b3c10b639fc7292142c082934f1b8126a5b
Brad McKinney <brad.mckin@gmail.com>
2018-07-20 15:46:04 -1000

    displays data on page (not styled yet)

--- js/app.js
+++ js/app.js
@@ -27,42 +27,26 @@ const checkStatus = (response) => {
 /*=========================
      PAGE CONSTRUCTION
 ==========================*/
-//Creates 12 identical divs for each employee that will be displayed
-//Each div is appended as a child of the 'directory' div
-let markup;
+let employeeData = {}
 
-const generateMarkup = () => {
+//Takes the fetched data and builds mark up around it to display on page.
+//Loops through the 12 returned objects and creates a div for each
+const generateMarkup = (data) => {
   for (i = 0; i < 12; i += 1) {
-    fetchData('https://randomuser.me/api/')
-      .then(res => markup = `
-        <div class="employee">
-          <img src="${res.results[0].picture.thumbnail}" class="photos">
-          <h3 class="name">${res.results[0].name.first} ${res.results[0].name.last}</h3>
-          <p class="email">${res.results[0].email}</p>
-          <p class="city">${res.results[0].location.city}</p>
-        </div>
-        `)
-
+    let markup = `
+      <div class="employee">
+        <img src="${data[i].picture.thumbnail}" class="photos">
+        <h3 class="name">${data[i].name.first} ${data[i].name.last}</h3>
+        <p class="email">${data[i].email}</p>
+        <p class="city">${data[i].location.city}</p>
+      </div>
+    `;
     directory.innerHTML += markup;
   }
 }
-generateMarkup();
 
-/*
-//Variables containing arrays, created from the img, h3, and p elements in employee divs
-const photos = document.querySelectorAll('.photos');
-const names = document.querySelectorAll('.name');
-const emails = document.querySelectorAll('.email');
-const cities = document.querySelectorAll('.city');
-
-for (i = 0; i < 12; i += 1) {
-  fetchData('https://randomuser.me/api/')
-    .then(res => photos[i].src = res.results[0].picture.thumbnail)
-    .then(res => names[i].textContent = res.results[0].name.first + " " + res.results[0].name.last)
-    .then(res => emails[i].textContent = res.results[0].email)
-    .then(res => cities[i].textContent = res.results[0].location.city)
-}
-*/
-
-fetchData('https://randomuser.me/api/')
-  .then(res => console.log(res.results[0].picture.thumbnail))
+//fetchData function is called, returning 12 objects from randomuser API
+//generateMarkup function is called, passing the returned data as an argument
+fetchData('https://randomuser.me/api/?results=12')
+  .then(res => employeeData = res.results)
+  .then(data => generateMarkup(data))
